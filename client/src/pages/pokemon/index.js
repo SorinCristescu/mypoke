c3fffe66fff4b3f5e2198f8eb59306d0f6cfb9b7
SorinCristescu <sorin.t.cristescu@gmail.com>
2020-11-23 20:42:36 +0100

    updates and cleanup

--- client/src/pages/pokemon/index.js
+++ client/src/pages/pokemon/index.js
@@ -1,5 +1,4 @@
-import React, { useEffect } from 'react';
-import PropTypes from 'prop-types';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getPokemon } from '../../redux/user/actions';
 
@@ -10,7 +9,6 @@ import Loader from '../../components/loader';
 // Styles
 import useStyles from './styles';
 import Grid from '@material-ui/core/Grid';
-import Container from '@material-ui/core/Container';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import Chip from '@material-ui/core/Chip';
@@ -20,67 +18,64 @@ const Pokemon = (props) => {
   const loading = useSelector((state) => state.user.loading);
   const pokemon = useSelector((state) => state.user.pokemon);
   const dispatch = useDispatch();
+  const [poke, setPoke] = useState({});
 
   const id = props.match.params.id;
 
   useEffect(() => {
     dispatch(getPokemon(id));
-  }, []);
+    setPoke(pokemon);
+  }, [dispatch, id, pokemon]);
+
+  const displayPokemon = () => {
+    const { base, type, urlImage, name } = poke;
+    const { HP, Attack, Defense, SpAttack, SpDefense, Speed } = base;
+    const dataSet = [HP, Attack, Defense, SpAttack, SpDefense, Speed];
+    return (
+      <Paper className={classes.paper}>
+        <Grid container spacing={3}>
+          <Grid
+            item
+            xs={6}
+            justify="center"
+            alignItems="center"
+            className={classes.gridItem}
+          >
+            <img src={urlImage} alt="pokemon" className={classes.image} />
+          </Grid>
+          <Grid
+            item
+            xs={6}
+            justify="center"
+            alignItems="center"
+            className={classes.gridItem}
+          >
+            <Typography variant="h4" align="center">
+              {name}
+            </Typography>
+            <div className={classes.chips}>
+              {type.map((chip, index) => (
+                <Chip
+                  key={index}
+                  className={classes.chip}
+                  color="primary"
+                  size="small"
+                  label={chip}
+                />
+              ))}
+            </div>
+            <Chart dataSet={dataSet} />
+          </Grid>
+        </Grid>
+      </Paper>
+    );
+  };
 
   if (loading) {
     return <Loader />;
-  } else if (!loading && pokemon) {
-    const { base, type, urlImage, name } = pokemon;
-    const { HP, Attack, Defense, SpAttack, SpDefense, Speed } = base;
-    const dataSet = [HP, Attack, Defense, SpAttack, SpDefense, Speed];
-
-    return (
-      <Container maxWidth="md" className={classes.root}>
-        <Paper className={classes.paper}>
-          <Grid container spacing={3}>
-            <Grid
-              item
-              xs={6}
-              justify="center"
-              alignItems="center"
-              className={classes.gridItem}
-            >
-              <img
-                src={urlImage}
-                alt="pokemon image"
-                className={classes.image}
-              />
-            </Grid>
-            <Grid
-              item
-              xs={6}
-              justify="center"
-              alignItems="center"
-              className={classes.gridItem}
-            >
-              <Typography variant="h4" align="center">
-                {name}
-              </Typography>
-              <div className={classes.chips}>
-                {type.map((chip, index) => (
-                  <Chip
-                    key={index}
-                    className={classes.chip}
-                    color="primary"
-                    size="small"
-                    label={chip}
-                  />
-                ))}
-              </div>
-              <Chart dataSet={dataSet} />
-            </Grid>
-          </Grid>
-        </Paper>
-      </Container>
-    );
+  } else {
+    return <div className={classes.root}>{displayPokemon()}</div>;
   }
 };
-
-Pokemon.propTypes = {};
 
 export default Pokemon;
