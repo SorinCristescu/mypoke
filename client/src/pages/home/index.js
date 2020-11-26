04775d787c2dfe3334a9563baf203e18ff874cd8
SorinCristescu <sorin.t.cristescu@gmail.com>
2020-11-26 05:08:02 +0100

    updates

--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -1,9 +1,9 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-import { Container, Grow, Typography, Button } from "@material-ui/core";
-import Logo from "../../assets/images/pikachu.svg";
-import useStyles from "./styles";
+import { Container, Grow, Typography, Button } from '@material-ui/core';
+import Logo from '../../assets/images/pikachu.svg';
+import useStyles from './styles';
 
 const Home = () => {
   const classes = useStyles();
@@ -14,7 +14,7 @@ const Home = () => {
       </Grow>
       <div className={classes.text}>
         <Typography variant="h2" component="h2" align="center">
-          Wellcome to MyPoke
+          Welcome to MyPoke
         </Typography>
         <Typography variant="h5" component="h5" align="center">
           You can explore now the pokemons world and start creating your own
--- client/src/pages/pokemons/index.js
+++ client/src/pages/pokemons/index.js
@@ -1,13 +1,13 @@
-import React, { useEffect, useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { getAllPokemons } from "../../redux/pokemons/actions";
-import { createAndUpdatePokeboard } from "../../redux/user/actions";
-import { capitalize } from "../../utils/helpers";
+import React, { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getAllPokemons } from '../../redux/pokemons/actions';
+import { createAndUpdatePokeboard } from '../../redux/user/actions';
+import { capitalize } from '../../utils/helpers';
 
-import List from "../../components/list";
-import Loader from "../../components/loader";
+import List from '../../components/list';
+import Loader from '../../components/loader';
 
-import useStyles from "./styles";
+import useStyles from './styles';
 import {
   Container,
   Typography,
@@ -15,10 +15,10 @@ import {
   InputBase,
   IconButton,
   Tooltip,
-} from "@material-ui/core";
-import SearchIcon from "@material-ui/icons/Search";
-import AtoZ from "../../assets/images/a-z.svg";
-import ZtoA from "../../assets/images/z-a.svg";
+} from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
+import AtoZ from '../../assets/images/a-z.svg';
+import ZtoA from '../../assets/images/z-a.svg';
 
 const Pokemons = (props) => {
   const classes = useStyles();
@@ -27,8 +27,8 @@ const Pokemons = (props) => {
   const user = useSelector((state) => state.auth.user);
   const dispatch = useDispatch();
   const path = props.match.path;
-  const [searchQuerry, setSearchQuerry] = useState("");
-  const [sortType, setSortType] = useState("asc");
+  const [searchQuerry, setSearchQuerry] = useState('');
+  const [sortType, setSortType] = useState('asc');
 
   useEffect(() => {
     dispatch(getAllPokemons());
@@ -44,7 +44,7 @@ const Pokemons = (props) => {
   }
 
   let sorted = pokemons.sort((a, b) => {
-    const isReversed = sortType === "asc" ? 1 : -1;
+    const isReversed = sortType === 'asc' ? 1 : -1;
     return isReversed * a.name.localeCompare(b.name);
   });
 
@@ -55,7 +55,7 @@ const Pokemons = (props) => {
   return (
     <div className={classes.root}>
       <Typography variant="h5" align="center">
-        Wellcome {capitalize(user.name)}
+        Welcome {capitalize(user.name)}
       </Typography>
       <Typography variant="body1" align="center">
         Now you can start choosing your favorite pokemons and add them to your
@@ -73,12 +73,12 @@ const Pokemons = (props) => {
         </div>
         <div className={classes.sort}>
           <Tooltip title="Sort A-Z" arrow>
-            <IconButton size="small" onClick={() => setSortType("asc")}>
+            <IconButton size="small" onClick={() => setSortType('asc')}>
               <img src={AtoZ} alt="ascending" className={classes.image} />
             </IconButton>
           </Tooltip>
-          <Tooltip title="Sort Z-Z" arrow>
-            <IconButton size="small" onClick={() => setSortType("des")}>
+          <Tooltip title="Sort Z-A" arrow>
+            <IconButton size="small" onClick={() => setSortType('des')}>
               <img src={ZtoA} alt="descending" className={classes.image} />
             </IconButton>
           </Tooltip>
